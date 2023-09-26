aa25f30168f48d97be4dd10ea0e4dc8c94883af4
zhiwei-dong <dongz.cn@outlook.com>
2023-09-26 20:26:48 +0800

    [llama]:update llama attn

--- torch_int/models/llama2.py
+++ torch_int/models/llama2.py
@@ -36,28 +36,25 @@ class Int8LlamaAttention(nn.Module):
     ):
         super().__init__()
         self.config = config
-        self.hidden_size = config.hidden_size
+        self.embed_dim = config.hidden_size
         self.num_heads = config.num_attention_heads
-        self.head_dim = self.hidden_size // self.num_heads
-        self.num_key_value_heads = config.num_key_value_heads
-        self.num_key_value_groups = self.num_heads // self.num_key_value_heads
-        self.max_position_embeddings = config.max_position_embeddings
-        self.rope_theta = config.rope_theta
-        if (self.head_dim * self.num_heads) != self.hidden_size:
+        self.head_dim = self.embed_dim // self.num_heads
+
+        if (self.head_dim * self.num_heads) != self.embed_dim:
             raise ValueError(
-                f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
+                f"embed_dim must be divisible by num_heads (got `embed_dim`: {self.embed_dim}"
                 f" and `num_heads`: {self.num_heads})."
             )
+
+        self.attention_weight_scale = 1.0
 
         self.qk_bmm = BMM_S8T_S8N_F32T(1.0)
         self.pv_bmm = BMM_S8T_S8N_S8T(1.0)
 
-        self.k_proj = W8A8B8O8Linear(self.hidden_size, self.hidden_size)
-        self.v_proj = W8A8B8O8Linear(self.hidden_size, self.hidden_size)
-        self.q_proj = W8A8B8O8Linear(self.hidden_size, self.hidden_size)
-        self.o_proj = W8A8BFP32OFP32Linear(self.hidden_size, self.hidden_size)
-
-        self._init_rope()
+        self.k_proj = W8A8B8O8Linear(self.embed_dim, self.embed_dim)
+        self.v_proj = W8A8B8O8Linear(self.embed_dim, self.embed_dim)
+        self.q_proj = W8A8B8O8Linear(self.embed_dim, self.embed_dim)
+        self.out_proj = W8A8BFP32OFP32Linear(self.embed_dim, self.embed_dim)
 
     @staticmethod
     @torch.no_grad()
@@ -78,7 +75,7 @@ class Int8LlamaAttention(nn.Module):
             module.k_proj, input_scale, k_output_scale)
         int8_module.v_proj = W8A8B8O8Linear.from_float(
             module.v_proj, input_scale, v_output_scale)
-        int8_module.o_proj = W8A8BFP32OFP32Linear.from_float(
+        int8_module.out_proj = W8A8BFP32OFP32Linear.from_float(
             module.o_proj, out_input_scale)
         int8_module.qk_bmm = BMM_S8T_S8N_F32T.from_scale(
             q_output_scale, k_output_scale)
@@ -88,151 +85,129 @@ class Int8LlamaAttention(nn.Module):
             1.0 / 127, v_output_scale, out_input_scale)
         return int8_module
 
-    def _init_rope(self):
-        if self.config.rope_scaling is None:
-            self.rotary_emb = LlamaRotaryEmbedding(
-                self.head_dim,
-                max_position_embeddings=self.max_position_embeddings,
-                base=self.rope_theta,
-            )
-        else:
-            scaling_type = self.config.rope_scaling["type"]
-            scaling_factor = self.config.rope_scaling["factor"]
-            if scaling_type == "linear":
-                self.rotary_emb = LlamaLinearScalingRotaryEmbedding(
-                    self.head_dim,
-                    max_position_embeddings=self.max_position_embeddings,
-                    scaling_factor=scaling_factor,
-                    base=self.rope_theta,
-                )
-            elif scaling_type == "dynamic":
-                self.rotary_emb = LlamaDynamicNTKScalingRotaryEmbedding(
-                    self.head_dim,
-                    max_position_embeddings=self.max_position_embeddings,
-                    scaling_factor=scaling_factor,
-                    base=self.rope_theta,
-                )
-            else:
-                raise ValueError(f"Unknown RoPE scaling type {scaling_type}")
-
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
         return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
 
+    @torch.no_grad()
     def forward(
         self,
         hidden_states: torch.Tensor,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
+        key_value_states: Optional[torch.Tensor] = None,
+        position_ids = None,
         past_key_value: Optional[Tuple[torch.Tensor]] = None,
-        output_attentions: bool = False,
+        attention_mask: Optional[torch.Tensor] = None,
+        layer_head_mask: Optional[torch.Tensor] = None,
         use_cache: bool = False,
+        output_attentions: bool = False,
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-        bsz, q_len, _ = hidden_states.size()
+        """Input shape: Batch x Time x Channel"""
+        # if key_value_states are provided this layer is used as a cross-attention layer
+        # for the decoder
+        is_cross_attention = key_value_states is not None
 
-        # if self.config.pretraining_tp > 1:
-        #     key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
-        #     query_slices = self.q_proj.weight.split(
-        #         (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
-        #     )
-        #     key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
-        #     value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
+        bsz, tgt_len, _ = hidden_states.size()
 
-        #     query_states = [F.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)]
-        #     query_states = torch.cat(query_states, dim=-1)
-
-        #     key_states = [F.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)]
-        #     key_states = torch.cat(key_states, dim=-1)
-
-        #     value_states = [F.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)]
-        #     value_states = torch.cat(value_states, dim=-1)
-
-        # else:
+        # get query proj
         query_states = self.q_proj(hidden_states)
-        key_states = self.k_proj(hidden_states)
-        value_states = self.v_proj(hidden_states)
-
-
-        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-
-        proj_shape = (bsz * self.num_heads, q_len, self.head_dim)
-        query_states = query_states.reshape(*proj_shape)
-        key_states = key_states.reshape(*proj_shape)
-        value_states = value_states.reshape(*proj_shape)
-
-        kv_seq_len = key_states.shape[-2]
-        if past_key_value is not None:
-            kv_seq_len += past_key_value[0].shape[-2]
-        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
-
-        if past_key_value is not None:
+        # get key, value proj
+        if is_cross_attention and past_key_value is not None:
+            # reuse k,v, cross_attentions
+            key_states = past_key_value[0]
+            value_states = past_key_value[1]
+        elif is_cross_attention:
+            # cross_attentions
+            key_states = self._shape(self.k_proj(key_value_states), -1, bsz)
+            value_states = self._shape(self.v_proj(key_value_states), -1, bsz)
+        elif past_key_value is not None:
             # reuse k, v, self_attention
+            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
+            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
             key_states = torch.cat([past_key_value[0], key_states], dim=2)
             value_states = torch.cat([past_key_value[1], value_states], dim=2)
+        else:
+            # self_attention
+            key_states = self._shape(self.k_proj(hidden_states), -1, bsz)
+            value_states = self._shape(self.v_proj(hidden_states), -1, bsz)
 
-        past_key_value = (key_states, value_states) if use_cache else None
+        past_key_value = (key_states, value_states)
 
-        # repeat k/v heads if n_kv_heads < n_heads
-        ori_shape = (bsz, self.num_heads, q_len, self.head_dim)
-        query_states = query_states.reshape(*ori_shape)
-        key_states = key_states.reshape(*ori_shape)
-        value_states = value_states.reshape(*ori_shape)
-        key_states = repeat_kv(key_states, self.num_key_value_groups)
-        value_states = repeat_kv(value_states, self.num_key_value_groups)
+        proj_shape = (bsz * self.num_heads, -1, self.head_dim)
+        query_states = self._shape(
+            query_states, tgt_len, bsz).view(*proj_shape)
+        key_states = key_states.view(*proj_shape)
+        value_states = value_states.view(*proj_shape)
 
-        query_states = query_states.reshape(*proj_shape)
-        key_states = key_states.reshape(*proj_shape)
-        value_states = value_states.reshape(*proj_shape)
+        src_len = key_states.size(1)
+        attn_weights = self.qk_bmm(query_states, key_states)
 
-        attn_weights = self.qk_bmm(query_states, key_states) / math.sqrt(self.head_dim)
-
-        attn_weights = attn_weights.reshape(bsz, self.num_heads, q_len, kv_seq_len)
-        # import pdb; pdb.set_trace()
-        if attn_weights.size() != (bsz, self.num_heads, q_len, kv_seq_len):
+        if attn_weights.size() != (bsz * self.num_heads, tgt_len, src_len):
             raise ValueError(
-                f"Attention weights should be of size {(bsz, self.num_heads, q_len, kv_seq_len)}, but is"
+                f"Attention weights should be of size {(bsz * self.num_heads, tgt_len, src_len)}, but is"
                 f" {attn_weights.size()}"
             )
 
         if attention_mask is not None:
-            if attention_mask.size() != (bsz, 1, q_len, kv_seq_len):
+            if attention_mask.size() != (bsz, 1, tgt_len, src_len):
                 raise ValueError(
-                    f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
+                    f"Attention mask should be of size {(bsz, 1, tgt_len, src_len)}, but is {attention_mask.size()}"
                 )
-            # attn_weights = attn_weights + attention_mask
-            #! add by dongz, delete attention_mask
-            attn_weights = attn_weights
+            attn_weights = attn_weights.view(
+                bsz, self.num_heads, tgt_len, src_len) + attention_mask
+            attn_weights = torch.max(attn_weights, torch.tensor(
+                torch.finfo(attn_weights.dtype).min))
+            attn_weights = attn_weights.view(
+                bsz * self.num_heads, tgt_len, src_len)
 
-        # upcast attention to fp32
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-        # attn_weights.mul_(127).round_()
-        # attn_weights = attn_weights.to(torch.int8)
-        attn_weights = attn_weights.reshape(bsz * self.num_heads, q_len, kv_seq_len)
-        attn_output = self.pv_bmm(attn_weights, value_states.transpose(1, 2))
-        attn_output = attn_output.reshape(bsz, self.num_heads, q_len, self.head_dim)
+        attn_probs = nn.functional.softmax(attn_weights, dim=-1)
 
-        if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+        if layer_head_mask is not None:
+            if layer_head_mask.size() != (self.num_heads,):
+                raise ValueError(
+                    f"Head mask for a single layer should be of size {(self.num_heads,)}, but is"
+                    f" {layer_head_mask.size()}"
+                )
+            attn_probs = layer_head_mask.view(
+                1, -1, 1, 1) * attn_probs.view(bsz, self.num_heads, tgt_len, src_len)
+            attn_probs = attn_probs.view(
+                bsz * self.num_heads, tgt_len, src_len)
+
+        if output_attentions:
+            # this operation is a bit awkward, but it's required to
+            # make sure that attn_weights keeps its gradient.
+            # In order to do so, attn_weights have to be reshaped
+            # twice and have to be reused in the following
+            attn_probs_reshaped = attn_probs.view(
+                bsz, self.num_heads, tgt_len, src_len)
+            attn_probs = attn_probs_reshaped.view(
+                bsz * self.num_heads, tgt_len, src_len)
+        else:
+            attn_probs_reshaped = None
+
+        # (A_row V_row)_row = (A_row V_col ^T)_row
+        attn_probs.mul_(127).round_()
+        attn_probs = attn_probs.to(torch.int8)
+
+        value_states = value_states.transpose(1, 2).contiguous()
+        # import pdb; pdb.set_trace()
+        attn_output = self.pv_bmm(attn_probs, value_states)
+
+        if attn_output.size() != (bsz * self.num_heads, tgt_len, self.head_dim):
             raise ValueError(
-                f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                f"`attn_output` should be of size {(bsz, self.num_heads, tgt_len, self.head_dim)}, but is"
                 f" {attn_output.size()}"
             )
 
-        attn_output = attn_output.transpose(1, 2).contiguous()
-        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+        attn_output = attn_output.view(
+            bsz, self.num_heads, tgt_len, self.head_dim)
+        attn_output = attn_output.transpose(1, 2)
 
-        if self.config.pretraining_tp > 1:
-            attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
-            o_proj_slices = self.o_proj.weight.split(self.hidden_size // self.config.pretraining_tp, dim=1)
-            attn_output = sum([F.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)])
-        else:
-            attn_output = self.o_proj(attn_output)
+        # Use the `embed_dim` from the config (stored in the class) rather than `hidden_state` because `attn_output` can be
+        # partitioned aross GPUs when using tensor-parallelism.
+        attn_output = attn_output.reshape(
+            bsz, tgt_len, self.embed_dim).contiguous()
+        attn_output = self.out_proj(attn_output)
 
-        if not output_attentions:
-            attn_weights = None
-
-        return attn_output, attn_weights, past_key_value
+        return attn_output, attn_probs_reshaped, past_key_value
 
 class Int8LlamaMLP(nn.Module):
     def __init__(self, config: LlamaConfig):
